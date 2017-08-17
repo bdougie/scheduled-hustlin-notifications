1bf81631ee5a406ac05ddb622a01949cf4a5d711
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-08-16 19:17:45 -0700

    working emails

--- handler.js
+++ handler.js
@@ -1,20 +1,25 @@
 'use strict';
 
-const getHustlin = require('./lib/hustlin');
+const axios = require('axios');
 const sendEmail = require('./lib/email');
 
 const emailRecpient = process.env.RECIPIENT;
 const emailSubject = 'There is baseball today!';
+const hustlinEndpoint = 'https://jzulvmdhac.execute-api.us-west-1.amazonaws.com/dev/today'
 
 module.exports.run = (event, context, callback) => {
-  getHustlin()
+  axios.get(hustlinEndpoint)
     .then((hustlin) => { // eslint-disable-line arrow-body-style
-      return sendEmail(emailRecpient, emailSubject, JSON.stringify(hustlin));
+      if (hustlin.data.games.length > 1) {
+        return sendEmail(emailRecpient, emailSubject, "http://hustlin.netlify.com/");
+      }
+      return () => {};
     })
     .then(() => {
       callback(null, { success: true });
     })
     .catch((error) => {
+      console.log(error)
       callback(error, { success: false });
     });
 };
